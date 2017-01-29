73aa182d91bd969b157ed3a7c05d8d15c8790b28
Tiago Matos <tiagoms88@gmail.com>
2017-01-29 03:59:17 -0300

    esqueleto do projeto

---
+++ fpa_fir/Debug/dependencias/lib/bsl/subdir_vars.mk
@@ -0,0 +1,48 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../dependencias/lib/bsl/ezdsp5502.c \
+../dependencias/lib/bsl/ezdsp5502_gpio.c \
+../dependencias/lib/bsl/ezdsp5502_i2c.c \
+../dependencias/lib/bsl/ezdsp5502_i2cgpio.c \
+../dependencias/lib/bsl/ezdsp5502_mcbsp.c 
+
+OBJS += \
+./dependencias/lib/bsl/ezdsp5502.obj \
+./dependencias/lib/bsl/ezdsp5502_gpio.obj \
+./dependencias/lib/bsl/ezdsp5502_i2c.obj \
+./dependencias/lib/bsl/ezdsp5502_i2cgpio.obj \
+./dependencias/lib/bsl/ezdsp5502_mcbsp.obj 
+
+C_DEPS += \
+./dependencias/lib/bsl/ezdsp5502.pp \
+./dependencias/lib/bsl/ezdsp5502_gpio.pp \
+./dependencias/lib/bsl/ezdsp5502_i2c.pp \
+./dependencias/lib/bsl/ezdsp5502_i2cgpio.pp \
+./dependencias/lib/bsl/ezdsp5502_mcbsp.pp 
+
+OBJS__QTD += \
+".\dependencias\lib\bsl\ezdsp5502.obj" \
+".\dependencias\lib\bsl\ezdsp5502_gpio.obj" \
+".\dependencias\lib\bsl\ezdsp5502_i2c.obj" \
+".\dependencias\lib\bsl\ezdsp5502_i2cgpio.obj" \
+".\dependencias\lib\bsl\ezdsp5502_mcbsp.obj" 
+
+C_DEPS__QTD += \
+".\dependencias\lib\bsl\ezdsp5502.pp" \
+".\dependencias\lib\bsl\ezdsp5502_gpio.pp" \
+".\dependencias\lib\bsl\ezdsp5502_i2c.pp" \
+".\dependencias\lib\bsl\ezdsp5502_i2cgpio.pp" \
+".\dependencias\lib\bsl\ezdsp5502_mcbsp.pp" 
+
+C_SRCS_QUOTED += \
+"../dependencias/lib/bsl/ezdsp5502.c" \
+"../dependencias/lib/bsl/ezdsp5502_gpio.c" \
+"../dependencias/lib/bsl/ezdsp5502_i2c.c" \
+"../dependencias/lib/bsl/ezdsp5502_i2cgpio.c" \
+"../dependencias/lib/bsl/ezdsp5502_mcbsp.c" 
+
+
---
+++ fpa_fir/Debug/main.pp
@@ -0,0 +1,85 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/c5500/include/stdio.h
+main.obj: C:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/c5500/include/linkage.h
+main.obj: C:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/c5500/include/stdarg.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/include/ezdsp5502.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/_csl.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_chiphal.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_std.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/tistdtypes.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_stdhal.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_datasz.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_chipdat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_icachdat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_irqdat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_datdat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_dmadat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_dmadatB.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_emifdat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_gpiodat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_gpio5502dat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_plldat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_mcbspdat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_pwrdat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_timdat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_gptdat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_bctdat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_errdat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_mmcdat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_wdtimdat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_rtcdat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_time.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_i2cdat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_uartdat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_csldat.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_cslhal.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_datasz.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_irq.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/_csl.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_irqhal.h
+main.obj: C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_err.h
+
+../main.c: 
+C:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/c5500/include/stdio.h: 
+C:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/c5500/include/linkage.h: 
+C:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/c5500/include/stdarg.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/include/ezdsp5502.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/_csl.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_chiphal.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_std.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/tistdtypes.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_stdhal.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_datasz.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_chipdat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_icachdat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_irqdat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_datdat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_dmadat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_dmadatB.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_emifdat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_gpiodat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_gpio5502dat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_plldat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_mcbspdat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_pwrdat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_timdat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_gptdat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_bctdat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_errdat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_mmcdat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_wdtimdat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_rtcdat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_time.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_i2cdat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_uartdat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_csldat.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_cslhal.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_datasz.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_irq.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/_csl.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_irqhal.h: 
+C:/Documents\ and\ Settings/Tiago/Desktop/LAB3/fpa_fir/dependencias/C55xxCSL/include/csl_err.h: 
